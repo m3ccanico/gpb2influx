0073923a281c25c3da54b1fd278d1bc3704ce440
Roman Ammann <m3ccanico@xxo.ch>
2018-05-06 17:43:02 +1200

    inital commit

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # gpb2influx
+
 Receives JunOS analytics data (interface statistic encoded in Google Protocol Buffers), unpacks them, and writes them into InfluxDB.
+
+## Install libraries
+
+```bash
+pip3 install influxdb protobuf
+```
+
+## Create protocol buffer parsers
+
+Download the Google Protocol Buffer (GPB) specification (i.e. `.proto` files) for the JunOS device from Juniper: https://www.juniper.net/support/downloads/. It appears that not all devices have corresponding GPB specificatio files (Juniper calls them *JUNOS Telemetry Interface Data Model Files*). I've used the ones from the [MX204](https://www.juniper.net/support/downloads/?p=mx204).
+
+The following command creates `logical_port.pb2.py` and `telemetry_top_pb2.py`. The two files are also included in the repo and don't need to be regenerated. The command is just here to document how to generate them:
+
+```bash
+apt install protobuf-compiler
+protoc --proto_path=junos-telemetry-interface --python_out=. junos-telemetry-interface/telemetry_top.proto junos-telemetry-interface/logical_port.proto -I /usr/include
+```
+
+## Run
+
+nohup gpb2influx.py &
+```
---
+++ gpb2influx.py
@@ -0,0 +1,132 @@
+#!/usr/bin/env python3
+
+# In this example we only monitor queue depth and latency for interfaces
+
+#from influxdb.influxdb08 import client
+import logical_port_pb2 
+import telemetry_top_pb2 
+import socket
+import time
+import pytz
+
+from influxdb import InfluxDBClient
+
+from datetime import datetime
+
+import pprint
+pp = pprint.PrettyPrinter(indent=4)
+
+INFLUX_DB_PORT = 8086
+INFLUX_DB_IP = '172.31.40.72'
+INFLUX_DB_NAME = 'throughput'
+ANALYTICS_PORT = 50001
+
+localtz = pytz.timezone('Pacific/Auckland')
+
+def main():
+    #db = client.InfluxDBClient('localhost', INFLUX_DB_PORT, 'root', 'root', 'network')
+    influx = InfluxDBClient(INFLUX_DB_IP, INFLUX_DB_PORT, '', '', INFLUX_DB_NAME)
+    server(influx)
+    
+def server(influx):
+    #sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.bind(('0.0.0.0', ANALYTICS_PORT))
+
+    while True:
+        data, addr = sock.recvfrom(32000)
+        start_time = time.time()
+        
+        # create TelemetryStream object and parse data
+        stream = telemetry_top_pb2.TelemetryStream()
+        stream.ParseFromString(data)        
+        #print(stream)
+        
+        # convert timestamp into a timezone aware timestamp
+        ts = datetime.fromtimestamp(stream.timestamp/1000.0)
+        ts = localtz.localize(ts)
+        
+        measurements = []
+        
+        # convert extension into JuniperNetworks object
+        juniper_networks = stream.enterprise.Extensions[telemetry_top_pb2.juniperNetworks]
+        # convert extension into LogicalInterfaceExt object
+        logical_interfaces = juniper_networks.Extensions[logical_port_pb2.jnprLogicalInterfaceExt]
+        
+        # iterate over logical interfaces
+        for interface_info in logical_interfaces.interface_info:
+            
+            # split system id "<name>:<IP>"
+            system_name = stream.system_id.split(":")[0]
+            
+            # create measurement for received bytes
+            measurement = {
+                "measurement": "throughput_in",
+                "tags": {
+                    "host": system_name,
+                    "interface": interface_info.if_name,
+                },
+                "time": ts.strftime('%Y-%m-%dT%H:%M:%S%Z'),
+                "fields": {
+                    "value": interface_info.ingress_stats.if_octets
+                }
+            }
+            measurements.append(measurement)
+            
+            # create measurement for sent bytes
+            measurement = {
+                "measurement": "throughput_out",
+                "tags": {
+                    "host": system_name,
+                    "interface": interface_info.if_name,
+                },
+                "time": ts.strftime('%Y-%m-%dT%H:%M:%S%Z'),
+                "fields": {
+                    "value": interface_info.egress_stats.if_octets
+                }
+            }
+            measurements.append(measurement)
+            
+            # create measurement for drops if it exist
+            tail_drop = 0
+            red_drop = 0
+            for queue_info in interface_info.egress_queue_info:
+                tail_drop += queue_info.tail_drop_packets
+                red_drop += queue_info.red_drop_packets
+            
+            measurement = {
+                "measurement": "tail_drop_packets",
+                "tags": {
+                    "host": system_name,
+                    "interface": interface_info.if_name,
+                },
+                "time": ts.strftime('%Y-%m-%dT%H:%M:%S%Z'),
+                "fields": {
+                    "value": tail_drop
+                }
+            }
+            measurements.append(measurement)
+            
+            measurement = {
+                "measurement": "red_drop_packets",
+                "tags": {
+                    "host": system_name,
+                    "interface": interface_info.if_name,
+                },
+                "time": ts.strftime('%Y-%m-%dT%H:%M:%S%Z'),
+                "fields": {
+                    "value": red_drop
+                }
+            }
+            measurements.append(measurement)
+            
+            
+        # write to DB and reset buffer
+        influx.write_points(measurements)
+        measurements = []
+        
+        #print("{0:.3f} sec".format(time.time() - start_time))
+    
+    conn.close()
+
+main()
